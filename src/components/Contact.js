15e5f1c9a965510bb0c67a4ef4a24701f1296388
Nghia NH <nhnghia2104@gmail.com>
2021-05-25 20:18:23 +0700

    remove address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,6 @@
 import React from "react";
 
 class Contact extends React.Component {
-  iframe = () => {
-    return {
-      __html:
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d641.2294237363797!2d106.84166949407341!3d10.906927010143189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174df6f66d3fa67%3A0xf9822525c1181b3!2sCogeek!5e0!3m2!1sen!2s!4v1609666100433!5m2!1sen!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>',
-    };
-  };
-
   render() {
     return (
       <div className="cgk-contact" id="scroll-target-contact">
@@ -61,9 +54,7 @@ class Contact extends React.Component {
                 </a>
               </div>
             </div>
-            <div className="col-md-6">
-              <div dangerouslySetInnerHTML={this.iframe()} />
-            </div>
+            <div className="col-md-6"></div>
           </div>
         </div>
       </div>
